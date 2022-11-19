3555cb3138c1aedcf3499e43b478528d41e92314
londonbrown <londonchaim@gmail.com>
2022-11-19 05:56:24 -0800

    Add tasks

--- src/task/CreateMenuTask.ts
+++ src/task/CreateMenuTask.ts
@@ -1,36 +1,60 @@
-import SeparatorMenuItem from "../menu/SeparatorMenuItem";
 import {browser, Menus} from "webextension-polyfill-ts";
-import MenuItem from "../menu/MenuItem";
-import SearchMenuItem from "../menu/SearchMenuItem";
-import LinkMenuItem from "../menu/LinkMenuItem";
-import LoadMenuItemsTask from "./LoadMenuItemsTask";
+import MenuItem, {MenuItemType} from "../menu/MenuItem";
+import MenuCategory, {MenuCategoryType} from "../menu/MenuCategory";
+import MenuItemsDAO from "../dao/MenuItemsDAO";
 
-export default class BuildMenuTask {
-    private static readonly menuItems = this.getMenuItems()
+export default class CreateMenuTask {
 
     async run() {
-        console.log(`Building menu from MenuItems: ${BuildMenuTask.menuItems.toString()}`)
-        for (let menuItem of await BuildMenuTask.menuItems) {
-            console.log(`Creating menuItem with id: ${menuItem.props.id}`)
+        console.log("Removing any existing menu items before building menu")
+        await browser.menus.removeAll()
+        console.log("Retrieving menu categories and items")
+        const menuCategories = await MenuItemsDAO.getInstance().getCategories()
+        const menuItems = await MenuItemsDAO.getInstance().getItems()
+        console.log(`Building menu from ${menuCategories.length} categories and ${menuItems.length} items`)
+        for (let menuCategory of await MenuItemsDAO.getInstance().getCategories()) {
+            console.log(`Creating menuCategory with id: ${menuCategory.id}`)
             try {
-                if (menuItem instanceof SeparatorMenuItem) {
+                if (menuCategory.type == MenuCategoryType.SEPARATOR) {
                     browser.menus.create({
                         type: "separator",
-                        parentId: menuItem.props.parentId,
+                        parentId: menuCategory.parentId,
                         contexts: ["selection"]
                     })
                 } else {
                     browser.menus.create({
-                        id: menuItem.props.id,
-                        title: menuItem.props.title,
-                        parentId: menuItem.props.parentId,
-                        contexts: ["selection"],
-                        onclick: BuildMenuTask.onItemClick
-                    }, () => BuildMenuTask.onItemCreated(menuItem))
+                        id: menuCategory.id,
+                        title: menuCategory.title,
+                        parentId: menuCategory.parentId,
+                        contexts: ["selection"]
+                    })
                 }
             } catch (e) {
-                console.error(`Failed to create menu item: ${menuItem.props.id}`)
-                BuildMenuTask.onError(e)
+                console.error(`Failed to create menu category: ${menuCategory.id}`)
+                CreateMenuTask.onError(e)
+            }
+        }
+        for (let menuItem of await MenuItemsDAO.getInstance().getItems()) {
+            console.log(`Creating menuItem with id: ${menuItem.id}`)
+            try {
+                if (menuItem.type == MenuItemType.SEPARATOR) {
+                    browser.menus.create({
+                        type: "separator",
+                        parentId: menuItem.categoryId,
+                        contexts: ["selection"]
+                    })
+                } else {
+                    browser.menus.create({
+                        id: menuItem.id,
+                        title: menuItem.title,
+                        parentId: menuItem.categoryId,
+                        contexts: ["selection"],
+                        onclick: CreateMenuTask.onItemClick
+                    }, () => CreateMenuTask.onItemCreated(menuItem))
+                }
+            } catch (e) {
+                console.error(`Failed to create menu item: ${menuItem.id}`)
+                CreateMenuTask.onError(e)
             }
         }
     }
@@ -40,16 +64,16 @@ export default class BuildMenuTask {
         lastError ? console.error(`Error: ${lastError.message}`) : console.debug("Tab created successfully")
     }
 
-    private static onItemCreated(menuItem: MenuItem): string | number {
-        console.log(`Created item: ${menuItem.props.id}`)
-        return menuItem.props.id
+    private static onItemCreated(menuComponent: MenuItem|MenuCategory): string | number {
+        console.log(`Created menu component of type ${menuComponent.type}: ${menuComponent.id}`)
+        return menuComponent.id
     }
 
     private static openInNewTab(searchUrl: string, selectionText?: string) {
         let url = selectionText === undefined ? searchUrl : searchUrl.replace("%s", selectionText)
         browser.tabs.create({
             url
-        }).then(BuildMenuTask.onTabCreated, BuildMenuTask.onError)
+        }).then(CreateMenuTask.onTabCreated, CreateMenuTask.onError)
     }
 
     private static onError(error: Error) {
@@ -58,24 +82,19 @@ export default class BuildMenuTask {
 
     private static onItemClick(info: Menus.OnClickData) {
         const { menuItemId, selectionText } = info
-        let menuItem = BuildMenuTask.menuItems.then(value => value.find(menuItem => {
-            return menuItem.props.id == menuItemId
+
+        let menuItem = MenuItemsDAO.getInstance().getItems().then(value => value.find(menuItem => {
+            return menuItem.id == menuItemId
         }))
         menuItem.then(value => {
-            if (value instanceof SearchMenuItem) {
-                console.debug("menuItem is type SearchMenuItem")
-                BuildMenuTask.openInNewTab(value.props.searchUrl.trim(), selectionText.trim())
-            } else if (value instanceof LinkMenuItem) {
-                console.debug("menuItem is type LinkMenuItem")
-                BuildMenuTask.openInNewTab(value.props.url.trim())
+            console.debug(`menuItem is type ${value.type}`)
+            if (value.type == MenuItemType.SEARCH) {
+                CreateMenuTask.openInNewTab(value.url.trim(), selectionText.trim())
+            } else if (value.type == MenuItemType.LINK) {
+                CreateMenuTask.openInNewTab(value.url.trim())
             } else {
                 console.debug("menuItem is not interactive. Nothing to do.")
             }
         })
-    }
-
-    private static async getMenuItems(): Promise<Array<MenuItem>> {
-        const loadMenuItemsTask = new LoadMenuItemsTask()
-        return await loadMenuItemsTask.load()
     }
 }
